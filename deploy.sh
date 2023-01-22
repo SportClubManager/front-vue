2779bf7e97a7f90afab55705e1c5286e339a77c1
Mykyta <mr.mykyta.popov@gmail.com>
2023-01-22 17:36:01 -0500

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,6 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# place .nojekyll to bypass Jekyll processing
-# echo > .nojekyll
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git checkout -B main
 git add -A
@@ -24,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sportclubmanager/front-end.git main:gh-pages
+git push -f git@github.com:SportClubManager/front-end.git main:gh-pages
 
 cd -
