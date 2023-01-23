3d9c35cfcc0bc6b5b1db594eadca8b8126c0c6c6
Mykyta <mr.mykyta.popov@gmail.com>
2023-01-23 11:03:48 -0500

    add nav list

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,13 +1,16 @@
 <template>
-    <header class="flex justify-between p-2.5 mb-2.5 bg-zinc-700 shadow-amber-50 shadow-md shadow-teal-800 sticky top-0">
+    <header class="flex flex-wrap justify-between p-2.5 mb-2.5 bg-zinc-700 shadow-amber-50 shadow-md shadow-teal-800 sticky top-0">
         <h1 class="bg-zinc-700">Sport Club Manager</h1>
-        <menu-icon-component/>
+        <nav-component/>
     </header>
 </template>
 
 <script setup>
-import MenuIconComponent from '@/components/MenuIconComponent.vue';
+import NavComponent from '@/components/NavComponent.vue';
 </script>
 
 <style scoped>
+header {
+    transform-style: preserve-3d;
+}
 </style>
---
+++ src/components/NavComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="nav-icon" @click="toggleMenu" :class="{ active: isActive }">
+        <div class="flex overflow-hidden items-center justify-center transform transition-all duration-200">
+            <div class="flex flex-col bg-zinc-700 justify-between w-5 h-5 transform transition-all duration-300">
+                <div class="bg-teal-500 h-[2px] w-7 transform transition-all duration-300 origin-left delay-100"></div>
+                <div class="bg-teal-500 h-[2px] w-8 rounded transform transition-all duration-300 delay-75"></div>
+                <div class="bg-teal-500 h-[2px] w-7 transform transition-all duration-300 origin-left "></div>
+
+                <div
+                    class="absolute items-center justify-between transform transition-all duration-500 top-2.5 -translate-x-10 flex w-0">
+                    <div
+                        class="absolute bg-red-400 h-[2px] w-5 transform transition-all duration-500 rotate-0 delay-300"></div>
+                    <div
+                        class="absolute bg-red-400 h-[2px] w-5 transform transition-all duration-500 -rotate-0 delay-300"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <ul class="w-full border-t border-teal-600 mt-2.5 pt-2.5 text-right space-y-2.5" v-if="isActive">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Contacts</a></li>
+        <li><a href="#">Sign in</a></li>
+    </ul>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isActive = ref(false);
+const toggleMenu = () => {
+    isActive.value = !isActive.value;
+};
+</script>
+
+<style scoped>
+
+* {
+    @apply bg-zinc-700;
+}
+
+#nav-icon.active > div > div > div:nth-child(-n+3) {
+    @apply translate-y-6
+}
+
+#nav-icon.active > div > div > div:nth-child(4) {
+    @apply translate-x-0 w-12
+}
+
+#nav-icon.active > div > div > div:nth-child(4) > div:nth-child(1) {
+    @apply rotate-45
+}
+
+#nav-icon.active > div > div > div:nth-child(4) > div:nth-child(2) {
+    @apply -rotate-45
+}
+</style>
